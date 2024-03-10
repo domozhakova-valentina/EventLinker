0bbb19d33d9328d9a3577b477cdb4e9dc0591f2a
Ivan Gaideek <workgaideek37@gmail.com>
2024-03-10 14:50:26 +0300

    initial presentation of the site

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
+from waitress import serve
 from app.app import main_app
 from flask import render_template
+import logging
+
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.DEBUG)
 
 
 @main_app.route('/', defaults={'path': ''})
-@main_app.route('/a/<path:path>')
-@main_app.route('/u/<path:path>')
+# @main_app.route('/a/<path:path>')
+# @main_app.route('/u/<path:path>')
 def root(path):
-    return render_template('base.html', title='EventLinker')
+    return render_template('index.html', title='EventLinker')
+
+
+@main_app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html', title='Авторизация')
+
+
+@main_app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html', title='Регистрация')
 
 
 if __name__ == '__main__':
-    main_app.run(port=5100, host='127.0.0.1', debug=True)
+    # main_app.run(port=8000, host='127.0.0.1', debug=True)
+    serve(main_app, host="127.0.0.1", port=8000)
