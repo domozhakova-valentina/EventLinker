3f6fa5df86ac2bdef8508514c49ca93c5352c74c
Valentina <domozhakova.valya@gmail.com>
2024-03-18 22:43:56 +0300

    Обработка форм

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from waitress import serve
 from app.app import main_app
-from flask import render_template
+from flask import render_template, redirect
+from flask_login import LoginManager, login_user
 from form.login_form import LoginForm
 from form.register_form import RegisterForm
 from form.createEvent_form import CreateForm
+from data.users import User
+from data.events import Event
+from data.comments import Comment
 import logging
+from data import db_session
 
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.DEBUG)
+
+login_manager = LoginManager()
+login_manager.init_app(main_app)
 
 
 @main_app.route('/', defaults={'path': ''})
@@ -23,7 +31,12 @@ def login():
     """Страница авторизация"""
     form = LoginForm()
     if form.validate_on_submit():
-        pass
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
@@ -41,9 +54,26 @@ def create_event():
     '''Страница - форма создания мероприятия'''
     form = CreateForm()
     if form.validate_on_submit():
-        pass
+        db_sess = db_session.create_session()
+        event = Event()
+        event.photo = form.photo.data
+        event.mini_description = form.mini_description.data
+        event.description = form.description.data
+        event.create_date = form.create_date.data
+        current_user.events.append(events)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect('/')
     return render_template('create_event.html', title='Создание мероприятия', form=form)
 
+
+@main_app.route('/home_user', methods=['GET', 'POST'])
+def home_user():
+    '''Страница пользователя'''
+    return render_template('user_home.html', title='Страница пользователя')
+
+'''Строчка. чтобы создать базу данных'''
+db_session.global_init("db/event_linker.db")
 
 if __name__ == '__main__':
     # main_app.run(port=8000, host='127.0.0.1', debug=True)
