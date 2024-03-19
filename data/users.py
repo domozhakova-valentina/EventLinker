b8318a07e33a564d61e249179f868987990418b9
Valentina <domozhakova.valya@gmail.com>
2024-03-19 17:07:44 +0300

    Обработка форм регистрации, авторизации и создания событий

---
+++ data/users.py
@@ -0,0 +1,35 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy_imageattach.entity import Image, image_attachment
+
+
+class UserPicture(SqlAlchemyBase, Image):
+    '''Класс для добавления изображений'''
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('user.id'), primary_key=True)
+    user = orm.relationship('User')
+    __tablename__ = 'user_picture'
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'user'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    photo = image_attachment('UserPicture')
+    about = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    events = orm.relationship("Event", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
+import flask
 from waitress import serve
 from app.app import main_app
 from flask import render_template, redirect
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, current_user
 from form.login_form import LoginForm
 from form.register_form import RegisterForm
 from form.createEvent_form import CreateForm
 from data.users import User
 from data.events import Event
-from data.comments import Comment
 import logging
 from data import db_session
 
@@ -16,6 +16,12 @@ logger.setLevel(logging.DEBUG)
 
 login_manager = LoginManager()
 login_manager.init_app(main_app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @main_app.route('/', defaults={'path': ''})
@@ -45,7 +51,22 @@ def register():
     """Страница регистрации"""
     form = RegisterForm()
     if form.validate_on_submit():
-        pass
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form, message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data,
+            photo=flask.request.files.get('file', '')
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
 
@@ -56,11 +77,10 @@ def create_event():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         event = Event()
-        event.photo = form.photo.data
+        event.photo = flask.request.files.get('imagefile', '')
         event.mini_description = form.mini_description.data
         event.description = form.description.data
-        event.create_date = form.create_date.data
-        current_user.events.append(events)
+        current_user.events.append(event)
         db_sess.merge(current_user)
         db_sess.commit()
         return redirect('/')
@@ -71,6 +91,7 @@ def create_event():
 def home_user():
     '''Страница пользователя'''
     return render_template('user_home.html', title='Страница пользователя')
+
 
 '''Строчка. чтобы создать базу данных'''
 db_session.global_init("db/event_linker.db")
