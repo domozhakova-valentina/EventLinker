fedd7186cd508dd67728d8778963482fcfc7be49
Ivan Gaideek <workgaideek37@gmail.com>
2024-03-21 00:07:32 +0300

    started to implement likes

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 import flask
 from waitress import serve
 from app.app import main_app
-from flask import render_template, redirect, request
+
 from form.addComment_form import AddComment
-from flask import render_template, redirect
+from flask import render_template, redirect, jsonify
 from flask_login import LoginManager, login_user, current_user
 from form.login_form import LoginForm
 from form.register_form import RegisterForm
 from form.createEvent_form import CreateForm
 from data.users import User
 from data.events import Event
-from data.comments import Comment
 import logging
 from form.search_form import SearchForm
 from data import db_session
@@ -60,7 +59,6 @@ def login():
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html', message="Неправильный логин или пароль", form=form)
-        return redirect('/')
     return render_template('login.html', title='Авторизация', form=form)
 
 
@@ -70,18 +68,17 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация', form=form)
+            return render_template('register.html', title='Регистрация', form=form, message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация', form=form, message="Такой пользователь уже есть")
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
         user = User(
             name=form.name.data,
             email=form.email.data,
-            about=form.about.data
+            about=form.about.data,
+            photo=flask.request.files.get('file', '')
         )
-        file = request.files.get('imagefile', '')
-        if file:
-            user.photo = file.read()
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
@@ -96,9 +93,7 @@ def create_event():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         event = Event()
-        file = flask.request.files.get('imagefile', '')
-        if file:
-            event.photo = file.read()
+        event.photo = flask.request.files.get('imagefile', '')
         event.mini_description = form.mini_description.data
         event.description = form.description.data
         current_user.events.append(event)
@@ -125,35 +120,15 @@ def edit_profile():
     """Редактирование аккаунта пользователя"""
     form = RegisterForm()
     form.submit.label.text = 'Изменить'
-    if request.method == "GET":
-        if current_user:
-            form.name.data = current_user.name
-            form.email.data = current_user.email
-            form.about.data = current_user.about
-            # сделать, чтобы отображалась аватарка
+    # данные сurrent_user предварительно тут записать в form
     if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.id == current_user.id).first()
-        if user:
-            user.name = form.name.data
-            user.email = form.email.data
-            user.about = form.about.data
-            file = request.files.get('imagefile', '')
-            if file:
-                user.photo = file.read()
-            # подумать как быть с паролем
-            db_sess.commit()
         return redirect('/')
     return render_template('edit_profile.html', title='Редактирование профиля', form=form)
 
 
 @main_app.route('/delete_user/<int:id>', methods=['GET', 'POST'])
 def delete_user(id):
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == id).first()
-    if user:
-        db_sess.delete(user)
-        db_sess.commit()
+    '''Удаление пользователя'''
     return redirect('/')
 
 
@@ -162,15 +137,7 @@ def event(id):
     '''Просмотр события (мероприятия)'''
     form = AddComment()
     if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        event = db_sess.query(Event).filter(Event.id == id).first()
-        comment = Comment()
-        comment.text = form.text_comment.data
-        current_user.comments.append(comment)
-        event.comments.append(comment)
-        db_sess.merge(current_user)
-        db_sess.merge(event)
-        db_sess.commit()
+        # добавление комментария и перезагрузка
         return redirect(f'/event/{id}')
     return render_template('event.html', form=form, test='<a href="https://lyceum.yandex.ru/">Тест ссылка</a>')
 
@@ -178,5 +145,4 @@ def event(id):
 if __name__ == '__main__':
     '''Строчка. чтобы создать базу данных'''
     db_session.global_init("db/event_linker.db")
-    # main_app.run(port=8000, host='127.0.0.1', debug=True)
     serve(main_app, host="127.0.0.1", port=8000)
