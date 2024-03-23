1ee51675ef46cc7b469dca310eb0def24098f055
Dasha <Dashaaf2006@gmail.com>
2024-03-24 01:00:10 +0300

    api

---
+++ api/comments.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+from flask import jsonify
+from flask_restful import reqparse, abort, Resource
+
+from data import db_session
+from data.comments import Comment
+
+
+def abort_if_comment_not_found(comment_id):
+    session = db_session.create_session()
+    comment = session.query(Comment).get(comment_id)
+    if not comment:
+        abort(404, message=f"Comment {comment_id} not found")
+
+
+class CommentResource(Resource):
+    def get(self, comment_id):  # получение информации по комментарию
+        abort_if_comment_not_found(comment_id)
+        session = db_session.create_session()
+        comment = session.query(Comment).get(comment_id)
+        return jsonify({'comment': comment.to_dict(
+            only=('id', 'text', 'create_date', 'create_user', 'event_id'))})
+
+    def post(self, comment_id):  # обновление комментария
+        abort_if_comment_not_found(comment_id)
+        session = db_session.create_session()
+        comment = session.query(Comment).get(comment_id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('text', required=True)
+        comment.text = parser.parse_args()['text']
+        comment.create_date = datetime.now()
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+    def delete(self, comment_id):  # удаление комментария
+        abort_if_comment_not_found(comment_id)
+        session = db_session.create_session()
+        comment = session.query(Comment).get(comment_id)
+        session.delete(comment)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class CommentsListResource(Resource):
+    def post(self):  # создание нового комментария
+        parser = reqparse.RequestParser()
+        parser.add_argument('text', required=True)
+        parser.add_argument('create_user', required=True, type=int)
+        parser.add_argument('event_id', required=True, type=int)
+        args = parser.parse_args()
+        session = db_session.create_session()
+        comment = Comment()
+        comment.text = args['text']
+        comment.create_date = datetime.now()
+        comment.create_user = args['create_user']
+        comment.event_id = args['event_id']
+        session.add(comment)
+        session.commit()
+        return jsonify({'id': comment.id})
--- app/app.py
+++ app/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
+from flask_restful import Api
 from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
-from errors.handlers import errors
-
+from api.users import UserResource, UsersListResource
+from api.comments import CommentResource, CommentsListResource
+from api.events import EventResource, EventsListResource
 
 class MyApp(Flask):
     def __init__(self, *args, **kwargs):
         super(MyApp, self).__init__(*args, **kwargs)
         self.config['SECRET_KEY'] = '23vghtklbn4hj8900'
-        self.register_blueprint(errors)  # добавление своих ошибок 404, 403, 500
         # self.json_encoder = MyJSONEncoder
         # self.user_repo = SqliteUsersRepo("./db/redditclone.db")
         # # app.user_repo = InMemoryUsersRepo()
@@ -25,3 +26,14 @@ main_app = MyApp(__name__, static_folder="./../static")
 fs_store = HttpExposedFileSystemStore('userimages', 'images/')
 main_app.wsgi_app = fs_store.wsgi_middleware(main_app.wsgi_app)
 # последние две строчки нужны для возможности сохранять изображения
+
+api = Api(main_app)
+# для пользователей
+api.add_resource(UsersListResource, '/api/v2/users')
+api.add_resource(UserResource, '/api/v2/users/<int:user_id>')
+# для событий
+api.add_resource(EventsListResource, '/api/v2/events')
+api.add_resource(EventResource, '/api/v2/events/<int:event_id>')
+# для комментариев
+api.add_resource(CommentsListResource, '/api/v2/comments')
+api.add_resource(CommentResource, '/api/v2/comments/<int:comment_id>')
