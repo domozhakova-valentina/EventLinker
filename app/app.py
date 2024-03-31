6f7e23e7a6374ac04ab838853519dccd15465365
Ivan Gaideek <workgaideek37@gmail.com>
2024-03-31 19:34:29 +0300

    fixed git merge issues

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restful import Api
-from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
 from errors.handlers import errors
 from api.users import UserResource, UsersListResource
 from api.comments import CommentResource, CommentsListResource
@@ -13,23 +12,9 @@ class MyApp(Flask):
         super(MyApp, self).__init__(*args, **kwargs)
         self.config['SECRET_KEY'] = '23vghtklbn4hj8900'
         self.register_blueprint(errors)  # добавление своих ошибок 404, 403, 500
-        # self.json_encoder = MyJSONEncoder
-        # self.user_repo = SqliteUsersRepo("./db/redditclone.db")
-        # # app.user_repo = InMemoryUsersRepo()
-        # # self.post_repo = InMemoryPostsRepo()
-        # self.post_repo = SqlitePostsRepo("./db/redditclone.db")
-        # self.config['JWT_SECRET_KEY'] = 'super-secret'
-        # self.config['JWT_EXPIRES'] = timedelta(hours=24)
-        # self.config['JWT_HEADER_NAME'] = 'authorization'
-        # self.config['JWT_IDENTITY_CLAIM'] = 'user'
-        # self.api = Api(self)
-        # self.jwt = JWTManager(self)
 
 
 main_app = MyApp(__name__, static_folder="./../static")
-fs_store = HttpExposedFileSystemStore('userimages', 'images/')
-main_app.wsgi_app = fs_store.wsgi_middleware(main_app.wsgi_app)
-# последние две строчки нужны для возможности сохранять изображения
 api = Api(main_app)
 # для пользователей
 api.add_resource(UsersListResource, '/api/v2/users')
